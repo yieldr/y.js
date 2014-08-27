ce42fb3fc6666973c751007f659eae343f11869b
alexkappa <alex.kalyvitis@gmail.com>
2014-08-27 14:16:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# y
+
+[![Build Status](https://secure.travis-ci.org/alexkalyvitis/y.png?branch=master)](http://travis-ci.org/alexkalyvitis/y)
+
+It does this one thing really awesome! boyah!
---
+++ test/spec.js
@@ -0,0 +1,17 @@
+/**
+ * y
+ *
+ *    Library test
+ */
+
+'use strict'
+
+var assert = require('assert'),
+lib        = require('../lib/y');
+
+describe('Basic library test', function() {
+  it('should answer all questions with YO!', function() {
+    var answer = lib.y('Should I tickle this unicorn?');
+    assert.equal(answer,'YO!');
+  })
+})
