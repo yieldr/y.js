fa4539e253d63bbd01797d40074f1fa218fb5457
alexkappa <alex.kalyvitis@gmail.com>
2014-10-20 12:14:55 +0200

    porting conflict fixes from n branch back to master

---
+++ tag/tag.js
@@ -0,0 +1,16 @@
+(function(y, l, d, r) {
+  y['YieldrTrackingObject'] = r;
+  y[r] = y[r] || {
+    data: {},
+    set: function(name, value) {
+      this.data[name] = value;
+    }
+  };
+  if (!y[r].loaded) {
+    var s = l.createElement("script");
+    s.async = true;
+    s.src = d;
+    l.head.appendChild(s);
+    y[r].loaded = true;
+  }
+})(window, document, "cdn.254a.com/static/y.js", "y");
