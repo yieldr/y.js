ca81695ff739212b3677c27fad233194d305da78
alexkappa <alex.kalyvitis@gmail.com>
2015-01-20 23:40:17 +0100

    more tests and minor improvements

--- test/test.y.js
+++ test/test.y.js
@@ -3,30 +3,22 @@
  *
  * @author Alex Kalyvitis <alex.kalyvitis@yieldr.com>
  */
-'use strict'
+'use strict';
 
 var assert = chai.assert;
 
-// if (!HTMLElement.prototype.click) {
-//     HTMLElement.prototype.click = function() {
-//         var e = document.createEvent('MouseEvent');
-//         e.initMouseEvent('click', true, true, window, null, 0, 0, 0, 0, false, false, false, false, 0, null);
-//         this.dispatchEvent(e);
-//     };
-// }
-
 describe('y', function () {
-    it('should be defined', function() {
-        assert.isDefined(y)
-    })
-    it('should be an object', function() {
-        assert.typeOf(y, 'object')
-    })
-    describe('event', function() {
-        it('should be defined', function() {
-            assert.isDefined(y.elem)
-        })
-        it('should execute when event triggers', function() {
+    it('should be defined', function () {
+        assert.isDefined(y);
+    });
+    it('should be an object', function () {
+        assert.typeOf(y, 'object');
+    });
+    describe('event', function () {
+        it('should be defined', function () {
+            assert.isDefined(y.elem);
+        });
+        it('should execute when event triggers', function () {
             var executed = false;
             y.elem(['a#test', 'click', function (element) {
                 executed = true;
@@ -39,25 +31,25 @@ describe('y', function () {
             document.getElementById('test').dispatchEvent(event); // click it!
 
             assert.isTrue(executed);
-        })
-    })
+        });
+    });
     describe('dl', function () {
-        it('should be defined', function() {
-            assert.isDefined(y.dl)
-        })
-        it('should gather data layer properties', function() {
+        it('should be defined', function () {
+            assert.isDefined(y.dl);
+        });
+        it('should gather data layer properties', function () {
             y.dl([{a:{b:'x'}}, {foo: 'a.b'}, false]);
 
+        });
+    });
+    describe('callback', function () {
+        it('should be defined', function () {
+            assert.isDefined(y.callback);
         })
-    })
-    describe('callback', function() {
-        it('should be defined', function() {
-            assert.isDefined(y.callback)
+        it('should be a function', function () {
+            assert.typeOf(y.callback, 'function');
         })
-        it('should be a function', function() {
-            assert.typeOf(y.callback, 'function')
-        })
-        it('should place piggybacks', function() {
+        it('should place piggybacks', function () {
             var response = {
                 status: 'success',
                 data: {
@@ -73,18 +65,18 @@ describe('y', function () {
             // assert.operator(document.getElementsByTagName('img').length, '>=', 1);
             // assert.operator(document.getElementsByTagName('script').length, '>=', 1);
             // assert.operator(document.getElementsByTagName('iframe').length, '>=', 2);
-        })
+        });
     })
-    describe('fire', function() {
-        it('should be defined', function() {
-            assert.isDefined(y.fire)
-        })
-        it('should be a function', function() {
-            assert.typeOf(y.fire, "function")
-        })
-        it('should place a script tag', function() {
-            var element = y.fire({"foo":"bar"});
+    describe('fire', function () {
+        it('should be defined', function () {
+            assert.isDefined(y.fire);
+        });
+        it('should be a function', function () {
+            assert.typeOf(y.fire, 'function');
+        });
+        it('should place a script tag', function () {
+            var element = y.fire({'foo': 'bar'});
             assert.include(element.src, 'foo=bar');
-        })
-    })
+        });
+    });
 });
